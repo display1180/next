35c3984b468fe88d007c6bdc9ffbf0d91f08e26f
tubi55 <tubi55@nate.com>
2023-08-04 09:43:15 +0900

    구글 웹폰트 적용

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import styles from './Header.module.scss';
-console.log(styles);
 
 function Header() {
 	const router = useRouter();
--- components/SubLayout.jsx
+++ components/SubLayout.jsx
@@ -1,5 +1,10 @@
 import Head from 'next/head';
 import Header from './Header';
+import styles from './SubLayout.module.scss';
+import { Orbitron } from 'next/font/google';
+
+const orbitron = Orbitron({ subsets: ['latin'], weight: ['400', '500'], preload: true });
+console.log(orbitron);
 
 function SubLayout(props) {
 	return (
@@ -10,8 +15,11 @@ function SubLayout(props) {
 
 			<section>
 				<Header />
-				<h1>{props.name}</h1>
-				{props.children}
+
+				<div className={styles.subLayout}>
+					<h1 className={orbitron.className}>{props.name}</h1>
+					{props.children}
+				</div>
 			</section>
 		</>
 	);
