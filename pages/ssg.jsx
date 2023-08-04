07d11d907ca445a9bb271954c7ab4cf1dba75571
tubi55 <tubi55@nate.com>
2023-08-04 12:17:54 +0900

    ssg방식의 이해

--- pages/ssg.jsx
+++ pages/ssg.jsx
@@ -1,11 +1,19 @@
 import SubLayout from '@/components/SubLayout';
 
-function Ssg() {
+function Ssg(props) {
 	return (
 		<SubLayout name={'SSG'}>
 			<p>SSG 방식 테스트 페이지입니다.</p>
+			<h1>{props.now}</h1>
 		</SubLayout>
 	);
+}
+
+export async function getStaticProps() {
+	console.log('ssg');
+	return {
+		props: { now: performance.now() },
+	};
 }
 
 export default Ssg;
