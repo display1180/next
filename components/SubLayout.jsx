e0e4e1aab86dc84d8afe04e4dc32b6fadb75ea44
tubi55 <tubi55@nate.com>
2023-08-04 09:51:12 +0900

    한글 폰트 스타일 적용

--- components/SubLayout.jsx
+++ components/SubLayout.jsx
@@ -1,10 +1,12 @@
 import Head from 'next/head';
 import Header from './Header';
 import styles from './SubLayout.module.scss';
-import { Orbitron } from 'next/font/google';
+import { Orbitron, Noto_Sans_KR } from 'next/font/google';
 
 const orbitron = Orbitron({ subsets: ['latin'], weight: ['400', '500'], preload: true });
+const notoSans = Noto_Sans_KR({ subsets: ['latin'], weight: ['100', '300'], preload: true });
 console.log(orbitron);
+console.log(notoSans);
 
 function SubLayout(props) {
 	return (
@@ -18,6 +20,7 @@ function SubLayout(props) {
 
 				<div className={styles.subLayout}>
 					<h1 className={orbitron.className}>{props.name}</h1>
+					<p className={notoSans.className}>레이아웃 컨텐츠 페이지입니다.</p>
 					{props.children}
 				</div>
 			</section>
