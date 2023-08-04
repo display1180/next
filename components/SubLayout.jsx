d94c3ed1bf70bb18b89ed04cbba7cbef830691ed
tubi55 <tubi55@nate.com>
2023-08-04 10:20:19 +0900

    clsx로 클래스 복수개 적용

--- components/SubLayout.jsx
+++ components/SubLayout.jsx
@@ -2,11 +2,13 @@ import Head from 'next/head';
 import Header from './Header';
 import styles from './SubLayout.module.scss';
 import { Orbitron, Noto_Sans_KR } from 'next/font/google';
+import clsx from 'clsx';
+//import clsx from 'clsx';
+//import { clsx } from 'clsx';
 
 const orbitron = Orbitron({ subsets: ['latin'], weight: ['400', '500'], preload: true, variable: '--orbitron' });
 const notoSans = Noto_Sans_KR({ subsets: ['latin'], weight: ['100', '300'], preload: true, variable: '--notoSans' });
-console.log(orbitron);
-console.log(notoSans);
+const isClass = true;
 
 function SubLayout(props) {
 	return (
@@ -18,8 +20,8 @@ function SubLayout(props) {
 			<section>
 				<Header />
 
-				<div className={styles.subLayout}>
-					<h1 className={orbitron.className}>{props.name}</h1>
+				<div className={clsx(styles.subLayout, isClass && orbitron.className, notoSans.className)}>
+					<h1>{props.name}</h1>
 					<p className={notoSans.className}>레이아웃 컨텐츠 페이지입니다.</p>
 					{props.children}
 				</div>
