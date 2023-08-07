5ee534248242fd5e413e926a729c3aa2114ab982
tubi55 <tubi55@nate.com>
2023-08-07 15:32:01 +0900

    구글 웹폰트 적용 수정

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,16 +1,21 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import styles from './Header.module.scss';
+import { Orbitron, Noto_Sans_KR } from 'next/font/google';
+import clsx from 'clsx';
+
+const orbitron = Orbitron({ subsets: ['latin'], weight: ['400', '500'], preload: true });
+const notoSans = Noto_Sans_KR({ subsets: ['latin'], weight: ['100', '300'], preload: true });
 
 function Header() {
 	const router = useRouter();
 	const currentPath = router.pathname;
 
 	return (
-		<header id={styles.header}>
+		<header id={styles.header} className={orbitron.className}>
 			<h1>
 				<Link href='/' className={currentPath === '/' ? styles.on : ''}>
-					Logo
+					DCODELAB
 				</Link>
 			</h1>
 
--- components/SubLayout.jsx
+++ components/SubLayout.jsx
@@ -3,12 +3,9 @@ import Header from './Header';
 import styles from './SubLayout.module.scss';
 import { Orbitron, Noto_Sans_KR } from 'next/font/google';
 import clsx from 'clsx';
-//import clsx from 'clsx';
-//import { clsx } from 'clsx';
 
-const orbitron = Orbitron({ subsets: ['latin'], weight: ['400', '500'], preload: true, variable: '--orbitron' });
-const notoSans = Noto_Sans_KR({ subsets: ['latin'], weight: ['100', '300'], preload: true, variable: '--notoSans' });
-const isClass = true;
+const orbitron = Orbitron({ subsets: ['latin'], weight: ['400', '500'], preload: true });
+const notoSans = Noto_Sans_KR({ subsets: ['latin'], weight: ['100', '300'], preload: true });
 
 function SubLayout(props) {
 	return (
@@ -20,7 +17,7 @@ function SubLayout(props) {
 			<section>
 				<Header />
 
-				<div className={clsx(styles.subLayout)}>
+				<div className={clsx(styles.subLayout, orbitron.className, notoSans.className)}>
 					<h1>{props.name}</h1>
 					<p>레이아웃 컨텐츠 페이지입니다.</p>
 					{props.children}
