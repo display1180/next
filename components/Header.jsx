0b8cbd1931cedca676331ac48d90fe21ff335f30
tubi55 <tubi55@nate.com>
2023-08-09 09:43:11 +0900

    구글 웹폰트 적용

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,18 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import styles from './Header.module.scss';
+//nextjs 프레임웍에는 기본적으로 구글 웹폰트 시스템이 내장되어 있음
+//next자체적으로 구글웹폰트의 용량을 최적화 시켜서 페이지에 적용
+//비구조화할당으로 원하는 폰트 객체를 import
 import { Orbitron, Noto_Sans_KR } from 'next/font/google';
 import clsx from 'clsx';
 
+//Orbitron생성자로부터 사용할 객체를 반환 (subsets: 언어타입, weight: [사용할 폰트 굵기] , preload: 사전로딩유무)
+//weight값이 등록되지않으면 콘솔 오류
+//반환된 폰트객체 안에는 클래스명이 프로퍼티로 등록되어 있음
+//해당 폰트를 사용하고자 하는 부모요소에 해당객체의 클래스명을 등록하면 자식컴포넌트에서는 자유롭게 해당 폰트 사용가능
 const orbitron = Orbitron({ subsets: ['latin'], weight: ['400', '500'], preload: true });
-const notoSans = Noto_Sans_KR({ subsets: ['latin'], weight: ['100', '300'], preload: true });
+//const notoSans = Noto_Sans_KR({ subsets: ['latin'], weight: ['100', '300'], preload: true });
 
 function Header() {
 	const router = useRouter();
