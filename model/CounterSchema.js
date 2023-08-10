32593e093f025f5f5cf4535dec1866056841ed76
tubi55 <tubi55@nate.com>
2023-08-10 11:17:28 +0900

    카운터, 커뮤니티 스키마 생성

---
+++ model/CounterSchema.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+//mongoose의 Schema생성자 함수로 글 저장 객체에 적용될 강제 틀 적용 (스키마)
+const counterSchema = new mongoose.Schema(
+	{
+		name: String,
+		communityNum: Number,
+	},
+	{ collection: 'Counter' }
+);
+
+//스키마를 적용한 모델생성함수를 내보냄
+const Counter = mongoose.model('Counter', counterSchema);
+export { Counter };
