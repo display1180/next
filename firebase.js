ec75336c26815316e38589f7a84ab4d531ce10f8
tubi55 <tubi55@nate.com>
2023-08-11 11:03:42 +0900

    firebase추가

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyAtT6f4Bz8iTzQ6ucXRjyZAQebVEz49IOU',
+	authDomain: 'dcode2307.firebaseapp.com',
+	projectId: 'dcode2307',
+	storageBucket: 'dcode2307.appspot.com',
+	messagingSenderId: '111290350000',
+	appId: '1:111290350000:web:4c91b4bbc0224108287e75',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
